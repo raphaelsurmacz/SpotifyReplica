d76ec08c1640337b86f8426f339e982a0259ab8b
raphaelsurmacz <raphael.surmacz@gmail.com>
2025-02-14 12:17:48 -0300

    player ajusts

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay, faBackwardStep, faForwardStep } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Player = () => {
     return (
         <div className='player'>
             <div className="player__controllers">
-            <FontAwesomeIcon className='player__icon' icon={faBackwardStep} />
-            <FontAwesomeIcon className='player__icon player__icon--play' icon={faCirclePlay} />
-            <FontAwesomeIcon className='player__icon' icon={faForwardStep} />
+                <Link to="/song/1">
+                    <FontAwesomeIcon className='player__icon' icon={faBackwardStep} />
+                </Link>
+            
+                <FontAwesomeIcon className='player__icon player__icon--play' icon={faCirclePlay} />
+
+                <Link to="/song/2">
+                    <FontAwesomeIcon className='player__icon' icon={faForwardStep} />
+                </Link>
             </div>
 
             <div className="player__progress">
-                
+                <p>00:00</p>
+                <div className="player__bar">
+                    <div className="player__bar--progress">
+
+                    </div>
+                </div>
+                <p>2:30</p>
             </div>
         </div>
 
--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import Player from '../components/Player';
+import { Link } from 'react-router-dom';
 
 const Song = () => {
     return (
         <div className="song">
             <div className="song__container">
                 <div className="song__image-container">
-                    <img src="https://i.scdn.co/image/ab67616d00001e02d8bfb4567b1b5e5bc4da915b"
-                    alt="X song image" />
+                    <img src="https://i.scdn.co/image/ab67616d00001e02d8bfb4567b1b5e5bc4da915b" alt="X song image" />
                 </div>
 
             </div>
 
             <div className="song__bar">
                 <div className='song__artist-image'>
-                    <img src="https://i.scdn.co/image/ab67616d00001e02d8bfb4567b1b5e5bc4da915b"
-                    width={80}
-                    height={80}
-                    alt="Artist image" />
+                    <Link to="/artist/1">
+                        <img src="https://i.scdn.co/image/ab67616d00001e02d8bfb4567b1b5e5bc4da915b"
+                        width={80}
+                        height={80}
+                        alt="Artist image" />
+                    </Link>
                 </div>
 
                 <Player />
